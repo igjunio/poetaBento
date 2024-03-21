d804fa558656766b693883cb44cb9585daf701fa
Igor Junio <igorjunio.inf@gmail.com>
2024-03-21 19:02:27 -0300

    Envio das primerias páginas do poema
    Envio das primerias páginas do poema

--- assets/js/lerPoema.js
+++ assets/js/lerPoema.js
@@ -1,14 +1,17 @@
 // Lista de páginas disponíveis
 const paginas = [
   'arrependimento.html',
-  'bunker.html'
+  'bunker.html',
+  'calabouco.html',
+  'janine.html',
+  'muaythai.html'
   // Adicione mais páginas conforme necessário
 ];
 
 // Função para redirecionar para uma página aleatória
 function abrirPaginaAleatoria() {
   const paginaAleatoria = paginas[Math.floor(Math.random() * paginas.length)];
-  window.location.href = `/Users/Igor%20Junio/Desktop/Poeta%20Bento/Site/PoetaBento/poesia/${paginaAleatoria}`;
+  window.location.href = `poesia/${paginaAleatoria}`;
 }
 
 // Event listener para o botão
