ae4c10fc91474cb37bc7e30f15746d9f9fd52a27
Igor Junio <igorjunio.inf@gmail.com>
2024-03-22 18:14:04 -0300

    Update lerPoema.js

--- assets/js/lerPoema.js
+++ assets/js/lerPoema.js
@@ -13,9 +13,11 @@ var enderecoAtual = window.location.href;
 // Função para redirecionar para uma página aleatória
 function abrirPaginaAleatoria() {
   const paginaAleatoria = paginas[Math.floor(Math.random() * paginas.length)];
-  if(enderecoAtual === 'https://igjunio.github.io/poetaBento'){
+  if(enderecoAtual === 'https://igjunio.github.io/poetaBento/'){
     window.location.href = `poesia/${paginaAleatoria}`;
   }else if (enderecoAtual === 'https://igjunio.github.io/poetaBento/index.html'){
+    window.location.href = `poesia/${paginaAleatoria}`;
+  }else if (enderecoAtual === 'https://igjunio.github.io/poetaBento/#'){
     window.location.href = `poesia/${paginaAleatoria}`;
   }else{
     window.location.href = `${paginaAleatoria}`;
