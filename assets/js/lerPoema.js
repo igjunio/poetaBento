f1bd2193c51a8ec2d99e07270c39674694d56428
Igor Junio <igorjunio.inf@gmail.com>
2024-04-01 14:08:48 -0300

    Atualização do repositório
    Atualização do repositório

--- assets/js/lerPoema.js
+++ assets/js/lerPoema.js
@@ -1,30 +1,22 @@
 // Lista de páginas disponíveis
 const paginas = [
-  'arrependimento.html',
-  'bunker.html',
-  'calabouco.html',
-  'janine.html',
-  'muaythai.html',
-  'barracao.html',
-  'reforma.html',
-  'musica.html'
+  'arrependimento',
+  'bunker',
+  'calabouco',
+  'janine',
+  'muaythai',
+  'barracao',
+  'reforma',
+  'musica'
   // Adicione mais páginas conforme necessário
-];
-
-var enderecoAtual = window.location.href;
+]; 
 
 // Função para redirecionar para uma página aleatória
 function abrirPaginaAleatoria() {
   const paginaAleatoria = paginas[Math.floor(Math.random() * paginas.length)];
-  if(enderecoAtual === "https://igjunio.github.io/poetaBento/"){
+  if (enderecoAtual === "https://igjunio.github.io/poetaBento/" || enderecoAtual === "https://igjunio.github.io/poetaBento/index.html" || enderecoAtual === "https://igjunio.github.io/poetaBento/#" || enderecoAtual === "https://igjunio.github.io/poetaBento") {
     window.location.href = `poesia/${paginaAleatoria}`;
-  }else if (enderecoAtual === "https://igjunio.github.io/poetaBento/index.html"){
-    window.location.href = `poesia/${paginaAleatoria}`;
-  }else if (enderecoAtual === "https://igjunio.github.io/poetaBento/#"){
-    window.location.href = `poesia/${paginaAleatoria}`;
-  }else if (enderecoAtual === "https://igjunio.github.io/poetaBento"){
-    window.location.href = `poesia/${paginaAleatoria}`;
-  }else{
+  } else {
     window.location.href = `${paginaAleatoria}`;
   }
 }
