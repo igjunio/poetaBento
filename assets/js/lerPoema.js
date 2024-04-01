3276db10eae009488932e01bf1398ab6f7da16aa
Igor Junio <igorjunio.inf@gmail.com>
2024-04-01 14:24:51 -0300

    Update lerPoema.js
    Teste da nova lógica

--- assets/js/lerPoema.js
+++ assets/js/lerPoema.js
@@ -14,7 +14,7 @@ const paginas = [
 // Função para redirecionar para uma página aleatória
 function abrirPaginaAleatoria() {
   const paginaAleatoria = paginas[Math.floor(Math.random() * paginas.length)];
-  if (enderecoAtual === "https://igjunio.github.io/poetaBento/" || enderecoAtual === "https://igjunio.github.io/poetaBento/index.html" || enderecoAtual === "https://igjunio.github.io/poetaBento/#" || enderecoAtual === "https://igjunio.github.io/poetaBento") {
+  if ((enderecoAtual === "https://igjunio.github.io/poetaBento/") || (enderecoAtual === "https://igjunio.github.io/poetaBento/index.html") || (enderecoAtual === "https://igjunio.github.io/poetaBento/#") || (enderecoAtual === "https://igjunio.github.io/poetaBento")) {
     window.location.href = `poesia/${paginaAleatoria}`;
   } else {
     window.location.href = `${paginaAleatoria}`;
